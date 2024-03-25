03a76234dcd35e5b09c3fad241bd7a9758712b96
your_github_username <richesuche91@gmail.com>
2024-03-25 15:58:31 -0700

    management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
-#!/usr/bin/pup
-# Install a specific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# This Puppet manifest installs the requirements for Flask using pip3.
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
 
+# This Puppet manifest installs Flask version 2.1.0 using pip3.
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['Werkzeug'],
+}
