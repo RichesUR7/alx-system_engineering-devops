66442099f1acfb56c565d07d6e3ad676c4668c37
your_github_username <richesuche91@gmail.com>
2024-03-26 17:11:08 -0700

    SSHS

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
+
+file { 'ect/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
+
+}
