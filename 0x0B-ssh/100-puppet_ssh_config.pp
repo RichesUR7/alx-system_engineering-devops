9e011652855c278ae7a6f37263634995478a6ad3
your_github_username <richesuche91@gmail.com>
2024-03-26 17:18:42 -0700

    SSS

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,10 @@
-#!/usr/bin/env bash
-# using puppet to make changes to our configuration file
+# This Puppet manifest configures the SSH client.
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '  IdentityFile ~/.ssh/school',
+}
 
-file { 'ect/ssh/ssh_config':
-	ensure => present,
-
-content =>"
-
-	#SSH client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
-
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '  PasswordAuthentication no',
 }
